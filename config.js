f945e0dd79d8283db670d72bc958770c09960ac5
umajogi-pentest <216833233+umajogi-pentest@users.noreply.github.com>
2026-08-13 22:16:33 +0530

    Update GitHub token in config.js
    Shortened the GitHub token in the configuration.

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
     accessKeyId: "AKIAZFAKEKEYLAB12345",
     secretAccessKey: "zF4k3S3cr3tK3yF0rLab1234567890abcdEFGH",
   },
-  githubToken: "ghp_16C7e42F292c6912E7710c838347Ae178B4a29X",
+  githubToken: "ghp_16C7e42F292c6912E7710c838347Ae178B4a",
   db: { file: "./login.sqlite" },
   exposeStackTraces: true
 };
